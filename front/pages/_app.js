7a1dbbe46da566b2e864eda5cef63b22b9ef4884
seanstainability <seanstainability@gmail.com>
2021-08-05 18:01:36 +0900

    레이아웃 나누기, 글로벌 스타일 적용

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -2,6 +2,7 @@ import React from "react";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 import Head from "next/head";
 import wrapper from "../store/configureStore";
+import GlobalStyle from "../GlobalStyle";
 
 const App = ({ Component }) => {
   // 페이지들의 부모 컴포넌트
@@ -10,6 +11,7 @@ const App = ({ Component }) => {
       <Head>
         <title>DayScore</title>
       </Head>
+      <GlobalStyle />
       <Component />
     </>
   );
--- front/pages/index.js
+++ front/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import styled from "styled-components";
 
 import PostForm from "../components/PostForm";
 import PostCard from "../components/PostCard";
@@ -11,14 +12,23 @@ const Home = () => {
 
   return (
     <AppLayout>
-      <div style={{ margin: 20 }}>
-        {isLoggedIn && <PostForm />}
-        {mainPosts.map((c) => {
-          return <PostCard key={c.id} post={c} />;
-        })}
+      <div style={{ margin: 10, display: "flex" }}>
+        <Half>
+          {isLoggedIn && <PostForm />}
+          {mainPosts.map((c) => {
+            return <PostCard key={c.id} post={c} />;
+          })}
+        </Half>
+        <Half>another half</Half>
       </div>
     </AppLayout>
   );
 };
+
+const Half = styled.div`
+  width: 50%;
+  padding: 10px;
+  overflow-y: scroll;
+`;
 
 export default Home;
